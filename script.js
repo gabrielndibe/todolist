ddcfe30841469e110702c202ad8f3313cb89320a
gabriel. <gndibe1@gmail.com>
2025-07-03 17:31:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const taskInput = document.getElementById("task-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const taskList = document.getElementById("task-list");
+let tasks = [];
+function addTask () {
+    const taskText = taskInput.value.trim();
+    if (taskText === '') {
+        alert('Please enter a task');
+        return;
+    }
+    tasks.push(taskText);
+    const li = document.createElement('li');
+   li.className = 'task-item';
+
+    const span = document.createElement('span');
+    span.textContent = taskText;
+    li.appendChild(span);
+    
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = 'Delete';
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    taskInput.value = '';
+
+    deleteBtn.addEventListener("click", function () {
+        taskList.removeChild(li);
+        tasks = tasks.filter(task !== taskText);
+    });
+    
+}
+    addTaskBtn.addEventListener("click", addTask)
+
+    taskInput.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+    });
